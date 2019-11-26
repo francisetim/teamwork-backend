b0f70fb659462f93461219ea91320467042f6968
Francis Etim <38356374+francisetim@users.noreply.github.com>
2019-11-26 19:20:16 +0100

    Automated testing with Mocha

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import http from 'http';
-import app from './app';
+import app from '../src/app';
 
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
@@ -13,7 +13,7 @@ const normalizePort = (val) => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '4200');
 app.set('port', port);
 
 const server = http.createServer(app);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { pool } from './config';
+import { pool } from '../config';
 
 const app = express();
 
